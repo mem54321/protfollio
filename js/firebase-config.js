da82d8302a90947aa1ecc126270c0c20f7033a9f
mem54321 <maramhussin22@gmail.com>
2026-07-07 18:46:39 +0300

    Add files via upload

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,11 +1,11 @@
 // firebase-config.js - Configuration for Firebase
 window.firebaseConfig = {
-  apiKey: "AIzaSyAd6_kBNqg_KFzgOEcTkZJU5PBSmyhe_A4",
-  authDomain: "rawnaq-hamdan.firebaseapp.com",
-  projectId: "rawnaq-hamdan",
-  storageBucket: "rawnaq-hamdan.firebasestorage.app",
-  messagingSenderId: "182749597495",
-  appId: "1:182749597495:web:e2b194a3c5585c5be654b2"
+  apiKey: "AIzaSyBr2tqlVr--UarO9kKP0O7D-MLKDX6BoU4",
+  authDomain: "rawnaq-hamdan-1d2cc.firebaseapp.com",
+  projectId: "rawnaq-hamdan-1d2cc",
+  storageBucket: "rawnaq-hamdan-1d2cc.firebasestorage.app",
+  messagingSenderId: "98717852431",
+  appId: "1:98717852431:web:2443b7617d71cef54c147a"
 };
 
 // Firebase is now enabled - data will sync to cloud for all devices
